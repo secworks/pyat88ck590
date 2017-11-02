caf0d4afe4f05fe8559a3625947fe90a2c21d4ad
Joachim Strömbergson <joachim@secworks.se>
2017-11-02 15:52:44 +0100

    Adding code and file headers to make it into a regular, readable program that can be started.

--- src/pyat88ck590.py
+++ src/pyat88ck590.py
@@ -1,9 +1,38 @@
-# Code to read out and display info about all USB devices connected.
-# Code use PyUSB and working backend such as libusb.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#=======================================================================
+#
+# pyat88ck590.py
+# --------------
+# Code to talk to the Atmel/Microchip Crypto Authentication Eval
+# Kit USB dongle.
+#
+#=======================================================================
 
+import sys
 import usb.core
 import usb.util
 
-dev = usb.core.find(find_all=True)
-for d in dev:
-    print(d)
+
+def show_usb_devices():
+    dev = usb.core.find(find_all=True)
+    for d in dev:
+        print(d)
+
+
+def main():
+    show_usb_devices()
+
+
+#-------------------------------------------------------------------
+# __name__
+# Python thingy which allows the file to be run standalone as
+# well as parsed from within a Python interpreter.
+#-------------------------------------------------------------------
+if __name__=="__main__":
+    # Run the main function.
+    sys.exit(main())
+
+#=======================================================================
+# EOF pyat88ck590.py
+#=======================================================================
